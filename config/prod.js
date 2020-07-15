2211042e8cc72389a50c60bd9d1132d5333aafef
Gagan Singh <gagansingh.dev@outlook.com>
2020-07-15 17:21:14 -0400

    Bugfixes

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 // prod.js
 module.exports = {
    googleClientID: process.env.GOOGLE_CLIENT_ID,
-   googleClientSecret: process.enc.GOOGLE_CLIENT_SECRET,
+   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
    mongoURI: process.env.MONGO_URI,
    cookieKey: process.env.COOKIE_KEY,
 };
