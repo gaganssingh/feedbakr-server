ae642638570f19ab8158f8991e56002836e497dd
Gagan Singh <gagansingh.dev@outlook.com>
2020-07-15 17:11:13 -0400

    Finished auth flow

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,4 +12,15 @@ module.exports = (app) => {
    // Use the code received from the Google OAuth above
    // to get info about the user from google
    app.get("/auth/google/callback", passport.authenticate("google"));
+
+   // Logout
+   app.get("/api/logout", (req, res) => {
+      req.logout();
+      res.send(req.user);
+   });
+
+   // Test auth route
+   app.get("/api/current_user", (req, res) => {
+      res.send(req.user);
+   });
 };
